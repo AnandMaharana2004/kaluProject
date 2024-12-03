dcaf3ae4e03936be857bf81de1ddc80869f0ed31
AnandMaharana2004 <anandmaharana427@gmail.com>
2024-12-03 21:47:15 +0530

    file setup

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express"
+const app = express()
+import path  from "path"
+import "ejs"
+app.set("view engine", "ejs")
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/',(req, res)=>{
+    // res.send("hii")
+    res.render("index")
+})
+
+app.listen('3000')
+
+export default app
---
+++ readme.md
@@ -0,0 +1,5 @@
+the date is : 3-12-2004
+
+
+this project is i make for my friend and the work is like there is a home page and other pages one of the page is email sending 
+so now i work on it to send the email
